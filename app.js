15ee3ab76fa5b75ce1efbb6e81d94306b3af7522
jazzdemariner <122781850+jazzdemariner@users.noreply.github.com>
2023-03-26 23:44:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 console.log("my name is kylian mbappe")
+console.log("mu name is joshiya samuel")
 console.log("mu name is andre russell")
-console.log("mu name is andre russell")
-console.log("mu name is andre russell")
-console.log("mu name is andre russell")
-console.log("mu name is andre russell")
-console.log("mu name is andre russell")
+console.log("mu name is karim benzema")
+console.log("mu name is lionel messi")
+console.log("mu name is eden hazard")
+// console.log("mu name is andre russell")
 let a = 18;
 let b = 14;
 let c = a+b;
