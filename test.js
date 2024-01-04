526e817f3b08e0d5ecc269b067ddb2f113d033f9
josh <jazzdemariner@gmail.com>
2024-01-05 00:13:44 +0530

    dgfdfdf

--- test.js
+++ test.js
@@ -1,12 +1,1 @@
-console.log("welcome");
-console.log("hi how are you are you fine");
-console.log("fffffffffffffff");
-console.log("hi i am from sam/develop");
-console.log("welcome too the future this chsnges is done from josh/develop branch");
-console.log("hi how are you are you fine");
-console.log("fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
-console.log("hi i am from sam/develop");
-console.log("welcome too the future this chsnges is done from josh/develop branch");
-console.log("hi how are you are you fine");
-console.log("fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
-console.log("hi i am from sam/develop");
+console.log("go ahead");
